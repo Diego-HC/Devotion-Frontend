e41e502e18ae6572e8cd84b0cfdff10bdd554d51
alexajro <93551879+alexajro@users.noreply.github.com>
2024-04-13 22:31:23 -0600

    Started sidebar/navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -3,9 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   template: `
-    <p>
-      navbar works!
-    </p>
+    <nav class="
+    bg-white
+    p-4 fixed z-10 w-full shadow-lg">
+        <div class="min-w-screen mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-full items-center">
+          <!-- Aquí puedes colocar el contenido de tu navbar -->
+          <div class = "px-10">Logo</div>
+          <div>Opciones</div>
+        </div>
+      </div>
+    </nav>
   `
 })
 export class NavbarComponent {
