ec99cace8cda59af207157e9b5576554e0c8e207
UmActually <89704397+UmActually@users.noreply.github.com>
2024-04-16 13:05:35 -0600

    Icon changes

--- src/app/shared/icons/files-icon/files-icon.component.ts
+++ src/app/shared/icons/files-icon/files-icon.component.ts
@@ -6,6 +6,6 @@ import { Component, Input } from '@angular/core';
 })
 export class FilesIconComponent {
   @Input() fill: string = '#2a4365';
-  @Input() width: string = '45';
+  @Input() width: string = '43';
   @Input() height: string = '44';
 }
