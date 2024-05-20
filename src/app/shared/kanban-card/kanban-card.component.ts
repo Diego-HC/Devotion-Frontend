e6a93411e9cec344c3457ade36803c1d76800338
Andrea Badillo <A00833511@tec.mx>
2024-05-20 11:17:43 -0600

    we have task previews but backend is funny

--- src/app/shared/kanban-card/kanban-card.component.ts
+++ src/app/shared/kanban-card/kanban-card.component.ts
@@ -4,10 +4,10 @@ import {Router} from '@angular/router';
 @Component({
   selector: 'app-kanban-card',
   template: `
-  <div class = "flex w-11/12"> 
+  <div class = "flex w-11/12">
     <div class = "flex flex-col mb-3 w-full">
       <div class = "flex align-middle place-items-center mb-3">
-        <div class = "shadow-sm w-2 h-2 rounded-full border-gray-500 border-solid mr-1" 
+        <div class = "shadow-sm w-2 h-2 rounded-full border-gray-500 border-solid mr-1"
         [class.bg-notStarted]="taskState === 'notStarted'"
         [class.bg-inProgress]="taskState === 'inProgress'"
         [class.bg-inReview]="taskState === 'inReview'"
@@ -21,7 +21,7 @@ import {Router} from '@angular/router';
         [class.bg-done]="taskState === 'done'"> {{ tasks?.length}} </div>
       </div>
       <ng-container *ngFor="let task of tasks">
-      <div class = "flex bg-white shadow-md w-full border-gray-500 rounded-sm m-1  hover:bg-gray-50" (click)="navigateToTask(task.id)">
+      <div class = "flex bg-white shadow-md w-full border-gray-500 rounded-sm m-1  hover:bg-gray-50" (click)="showTaskPreview(task.id)">
         <div class = "w-1 h-fullrounded-sm"
         [class.bg-notStarted]="taskState === 'notStarted'"
         [class.bg-inProgress]="taskState === 'inProgress'"
@@ -35,13 +35,23 @@ import {Router} from '@angular/router';
       </ng-container>
     </div>
   </div>
+  <app-task-preview *ngIf="selectedTaskId" [taskID]="selectedTaskId" (closePreview)="closeTaskPreview()"></app-task-preview>
   `
 })
 export class KanbanCardComponent {
   @Input() taskState: string = '';
   @Input() tasks? : TaskKanban[] = [];
+  selectedTaskId: string | null = null;
 
   constructor(private router: Router) { }
+
+  showTaskPreview(taskId: string) {
+    this.selectedTaskId = taskId;
+  }
+
+  closeTaskPreview() {
+    this.selectedTaskId = null;
+  }
 
   navigateToTask(taskid: string){
     this.router.navigate(['/task', taskid]);
@@ -57,7 +67,7 @@ export class KanbanCardComponent {
         return 'En Revisión';
       case 'done':
         return 'Completado';
-      default: 
+      default:
         return 'Unknown';
     }
   }
@@ -72,7 +82,7 @@ export class KanbanCardComponent {
         return 'bg-inReview';
       case 'done':
         return 'bg-done';
-      default: 
+      default:
         return 'Unknown';
     }
   }
--- src/app/tasks/kanban/kanban.component.ts
+++ src/app/tasks/kanban/kanban.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, ViewChild, OnDestroy} from '@angular/core';
 import { ApiService } from "../../api.service";
+import { Subscription} from "rxjs";
+import {TaskPreviewComponent} from "../task-preview/task-preview.component";
+import { StoreService} from "../../store.service";
 
 @Component({
   selector: 'app-kanban',
@@ -12,16 +15,38 @@ import { ApiService } from "../../api.service";
   </div>
   `
 })
-export class KanbanComponent implements OnInit{
+export class KanbanComponent implements OnInit, OnDestroy {
   constructor(
-    protected api: ApiService
+    protected api: ApiService,
+    protected store: StoreService
   ) {}
 
   @Input() projectOrTaskId: string = '';
+  @ViewChild(TaskPreviewComponent) taskPreview!: TaskPreviewComponent;
+  private updateSubscription: Subscription = new Subscription();
+
   response? : KanbanView;
-  
+
 
   ngOnInit(): void {
+    const endpoint = `projects/${this.projectOrTaskId}/?view=kanban&partial=true`
+    this.api.get(endpoint).subscribe((response : KanbanView) => {
+      this.response = response;
+    });
+    this.updateSubscription = this.store.needsUpdate$.subscribe(needsUpdate => {
+      if (needsUpdate) {
+        this.fetchApi();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.updateSubscription) {
+      this.updateSubscription.unsubscribe();
+    }
+  }
+
+  fetchApi() {
     const endpoint = `projects/${this.projectOrTaskId}/?view=kanban&partial=true`
     this.api.get(endpoint).subscribe((response : KanbanView) => {
       this.response = response;
@@ -29,6 +54,6 @@ export class KanbanComponent implements OnInit{
   }
 
   navigateToTask(taskId: string) {
-    
+
   }
 }
