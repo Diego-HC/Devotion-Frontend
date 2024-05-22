ce8e729a317cab53c46a8b07dad358928a328639
Andrea Badillo <A00833511@tec.mx>
2024-05-22 12:18:10 -0600

    ola

--- src/app/tasks/task-create-edit-page/task-create-edit-page.component.spec.ts
+++ src/app/tasks/task-create-edit-page/task-create-edit-page.component.spec.ts
@@ -1,17 +1,77 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { TaskCreateEditPageComponent } from './task-create-edit-page.component';
+import {TaskCreateEditPageComponent} from './task-create-edit-page.component';
+import {HttpClientModule} from '@angular/common/http';
+import {OAuthModule} from 'angular-oauth2-oidc';
+import {Router} from '@angular/router';
+import {ApiService} from '../../api.service';
+import {StoreService} from '../../store.service';
 
 describe('TaskCreateEditPageComponent', () => {
   let component: TaskCreateEditPageComponent;
   let fixture: ComponentFixture<TaskCreateEditPageComponent>;
+  let mockRouter = {
+    navigateByUrl: jasmine.createSpy('navigateByUrl'),
+  };
+  const taskId = "b3045125-c471-4c40-bcc3-6ec1ca9edc9f";
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TaskCreateEditPageComponent]
+      declarations: [TaskCreateEditPageComponent],
+      imports: [HttpClientModule, OAuthModule.forRoot()],
+      providers: [
+        {
+          provide: Router,
+          useValue: mockRouter,
+        },
+        {
+          provide: ApiService,
+          useValue: {
+            get: (url: string) => {
+              return {
+                subscribe: () => {
+                  return [];
+                },
+              };
+            },
+            post: (url: string, data: any) => {
+              return {
+                subscribe: (fn: (value: any) => void) => {
+                  fn({
+                    id: taskId,
+                    name: 'Task Name',
+                    description: 'Task Description',
+                  });
+                },
+              };
+            },
+          },
+        },
+        {
+          provide: StoreService,
+          useValue: {
+            task: {
+              id: '',
+              name: '',
+              description: '',
+              startDate: '',
+              dueDate: '',
+              assignee: {
+                id: '',
+                name: '',
+                email: '',
+                isLeader: false
+              },
+              priority: 0,
+              status: 0,
+              parentProject: '',
+              parentTask: ''
+            },
+          }
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(TaskCreateEditPageComponent);
     component = fixture.componentInstance;
