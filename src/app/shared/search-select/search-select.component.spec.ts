3cd88817a3bece06ca77247df8a2df9d59af8df3
Andrea Badillo <A00833511@tec.mx>
2024-05-22 11:01:11 -0600

    testsH13

--- src/app/shared/search-select/search-select.component.spec.ts
+++ src/app/shared/search-select/search-select.component.spec.ts
@@ -1,17 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SearchSelectComponent } from './search-select.component';
+import {SearchSelectComponent} from './search-select.component';
+import {StoreService} from "../../store.service";
 
 describe('SearchSelectComponent', () => {
   let component: SearchSelectComponent;
   let fixture: ComponentFixture<SearchSelectComponent>;
+  const assigneeToSearch = "Andrea Guadalupe Badillo Ibarra";
+  const assigneeId = "c4c4d6d8-aeac-482b-9e15-4650b9be22f2";
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SearchSelectComponent]
+      declarations: [SearchSelectComponent],
+      providers: [
+        {
+          provide: StoreService,
+          useValue: {
+            membersPool: [
+              {
+                name: "Andrea Guadalupe Badillo Ibarra",
+                email: "A00833511@tec.mx",
+                isLeader: true,
+              },
+            ],
+          }
+        },
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(SearchSelectComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +37,17 @@ describe('SearchSelectComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // • CA1H13 - Personas disponibles: Al momento de dar click en el campo para seleccionar líderes o miembros, debe permitirte escribir y autocompletar la búsqueda.
+  it('should show suggestions when input is focused', () => {
+    component.searchInput = assigneeToSearch;
+    component.updateSuggestions(new KeyboardEvent('keyup'));
+    expect(component.suggestions.length).toBe(1);
+  });
+
+  // • CA2H13 - Límite de selección: La cantidad de miembros que se pueden seleccionar son: [1, ∞]
+  // it('should allow selecting a member', () => {
+  //   component.selectSuggestion({name: assigneeId});
+  //   expect(component.selection()).toContain({name: assigneeToSearch});
+  // });
 });
