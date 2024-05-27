92fe81e7fbfc64bba6501cc0d24bd6527fc669bd
soosw <A01625514@tec.mx>
2024-05-26 18:00:20 -0600

    ola

--- src/app/tasks/roadmap/roadmap.component.spec.ts
+++ src/app/tasks/roadmap/roadmap.component.spec.ts
@@ -1,14 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 import { RoadmapComponent } from './roadmap.component';
-
+import { ApiService } from '../../api.service';
 describe('RoadmapComponent', () => {
   let component: RoadmapComponent;
   let fixture: ComponentFixture<RoadmapComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [RoadmapComponent]
+      declarations: [RoadmapComponent],
+      providers: [ApiService],
     })
     .compileComponents();
     
